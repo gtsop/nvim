2c97ceb7c9615f17e94357e3ed907b5758b7cd5e
gtsop <gtsop+github@protonmail.com>
2025-08-09 14:39:18 +0300

    add more file formats for prettier

--- lua/components/formatter/controller.lua
+++ lua/components/formatter/controller.lua
@@ -5,7 +5,7 @@ function M.new()
   local self = setmetatable({}, M)
 
   vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.js", "*.jsx" },
+    pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.md", "*.mdx", "*.json" },
     callback = function()
       local view = vim.fn.winsaveview()
       vim.cmd([[%!prettier --stdin-filepath %]])

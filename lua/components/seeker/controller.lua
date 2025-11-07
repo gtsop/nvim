53156bb98817b03bcbb2a0926e4b32c0ac8d43e0
gtsop <gtsop+github@protonmail.com>
2025-11-07 22:23:27 +0200

    feat: seeker render result files

--- lua/components/seeker/controller.lua
+++ lua/components/seeker/controller.lua
@@ -10,10 +10,27 @@ function M.new(opts)
   local model = Model.new({ root_dir = opts.base_path })
   local view = View.new()
 
+  local window = nil
+  local group = nil
+
   function self.seek(opts)
     model.search(opts.args, function(results)
       view.render(results)
+      view.show()
     end)
+  end
+
+  function self.show()
+    if window then
+      vim.api.nvim_set_current_win(window)
+      return
+    end
+
+    view.show()
+
+    window = view.get_window()
+
+    group = vim.api.nvim_create_augroup(("seeker_%d"):format(window), { clear = true })
   end
 
   vim.api.nvim_create_user_command("SeekerFind", self.seek, { nargs = 1 })
--- lua/components/seeker/view.lua
+++ lua/components/seeker/view.lua
@@ -19,12 +19,28 @@ function M.new()
   vim.api.nvim_buf_set_option(buffer, "filetype", "seeker")
   vim.api.nvim_buf_set_option(buffer, "modifiable", false)
 
+  local window = nil
+
+  function self.show()
+    window = vim.api.nvim_open_win(buffer, true, {
+      relative = "",
+      split = "below",
+    })
+    vim.api.nvim_set_option_value("number", false, { win = window })
+    vim.api.nvim_set_option_value("relativenumber", false, { win = window })
+    vim.api.nvim_set_option_value("wrap", false, { win = window })
+  end
+
   function self.render(model)
     local rendered_lines = get_rendered_lines(model)
 
     vim.api.nvim_buf_set_option(buffer, "modifiable", true)
     vim.api.nvim_buf_set_lines(buffer, 0, -1, false, rendered_lines)
     vim.api.nvim_buf_set_option(buffer, "modifiable", false)
+  end
+
+  function self.get_window()
+    return window
   end
 
   return self

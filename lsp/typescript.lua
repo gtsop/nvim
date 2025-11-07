5a0c728ab860ecef830908570ee49289c6397d15
gtsop <gtsop+github@protonmail.com>
2025-11-07 19:51:01 +0200

    feat: use capabilities

--- lsp/typescript.lua
+++ lsp/typescript.lua
@@ -1,16 +1,17 @@
 return {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-  root_markers = { ".git", "package.json" },
-  init_options = {
-    preferences = {
-      includeCompletionsForModuleExports = true,
-      includeCompletionsWithInsertText = true,
-      includeAutomaticOptionalChainCompletions = true,
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+	root_markers = { ".git", "package.json" },
+	capabilities = vim.lsp.protocol.make_client_capabilities(),
+	init_options = {
+		preferences = {
+			includeCompletionsForModuleExports = true,
+			includeCompletionsWithInsertText = true,
+			includeAutomaticOptionalChainCompletions = true,
 
-      includeInlayParameterNameHints = "all",
-      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-      includeInlayFunctionParameterTypeHints = true,
-    }
-  }
+			includeInlayParameterNameHints = "all",
+			includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+			includeInlayFunctionParameterTypeHints = true,
+		},
+	},
 }
